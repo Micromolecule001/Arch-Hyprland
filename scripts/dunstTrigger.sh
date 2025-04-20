5ae578ad8d27eb1ac48294de13f2aa5407e2f12d
Micromolecule001 <dima.boyko.bd@gmail.com>
2025-04-21 00:39:37 +0300

    waybar + dunst

---
+++ scripts/dunstTrigger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+killall dunst
+notify-send "Hello World!" "Description: lorem ipsum one two three \n nothing to wrap cause of \n because its not working there, just regular wrapping. \n \n \n I was wrong!!!"
